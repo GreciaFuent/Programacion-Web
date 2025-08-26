3b644572012e0c99966fd9c4734486f7035ae862
GreciaFuent <greciafuentes307@gmail.com>
2025-08-25 20:22:04 -0600

    feat(task): add the states to the button to complete a task

--- HW-04/src/components/task.jsx
+++ HW-04/src/components/task.jsx
@@ -1,19 +1,30 @@
-import "../styles/task.css"
+import { useState } from "react";
+import "../styles/task.css";
 
 function Task() {
+  const [status, setStatus] = useState(false);
 
   return (
     <>
-      <div className='task'>
-        <div className="status">P</div>
+      <div className="task">
+
+        <button className={`status ${status ? "completed" : "pending"}`} 
+          onClick={() => setStatus(!status)}>
+          {status ? "C" : "P"}
+        </button>
+
         <div className="info">
           <h3 className="title-task">Tarea Progra Web</h3>
           <h5 className="date-time">26/08/2025</h5>
-          <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis inventore aliquam v</p>
+          <p className="description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis inventore aliquam v
+          </p>
         </div>
+        
       </div>
     </>
-  )
+  );
 }
 
-export default Task
+export default Task;
+
